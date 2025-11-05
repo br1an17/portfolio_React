863483bd1d45608a3cc93ea3564165efe226c330
br1an17 <brianzairpaez@gmail.com>
2025-11-05 19:37:06 -0300

    arreglamos el about

--- vite-project/src/components/aboutMe/AboutMe.jsx
+++ vite-project/src/components/aboutMe/AboutMe.jsx
@@ -10,18 +10,19 @@ export function AboutMe() {
       <div className="history-log">
         <section className="log-section">
           <h3>[RÉCORD] TRAYECTORIA</h3>
-          <p className="log-entry">2020 - INICIO: Descubrimiento de React y Node.js.</p>
-          <p className="log-entry">2022 - LEVEL UP: Desarrollo de proyectos Full-Stack.</p>
-          <p className="log-entry">2024 - RANK S: Especialización en UI/UX y Animación.</p>
+          <p className="log-entry">2022 - INICIO: Descubrimiento de React y Node.js.</p>
+          <p className="log-entry">2023 - LEVEL UP: Desarrollo de proyectos Full-Stack.</p>
+          <p className="log-entry">2025 - RANK S: inicio de tecnicatura en desarrollo de software.</p>
         </section>
 
         <section className="log-section">
           <h3>[POWER-UPS] TECNOLOGÍAS</h3>
           <ul className="skill-list">
-            <li>JavaScript (Master)</li>
-            <li>React / Redux / Zustand (Expert)</li>
+            <li>JavaScript (Advance)</li>
+            <li>React / Redux (Advance)</li>
             <li>Node.js / Express (Advanced)</li>
-            <li>CSS / Styled-Components (Guru)</li>
+            <li>Java (Intermediate)</li>
+            <li>CSS / Styled-Components (Advance)</li>
             <li>Bases de Datos (SQL/NoSQL) (Intermediate)</li>
           </ul>
         </section>
